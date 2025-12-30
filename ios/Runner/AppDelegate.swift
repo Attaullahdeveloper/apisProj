9403bb69aa30d22b27662381a6d080888d6a7c50
Attaullahdeveloper <attaullahdeveloper46@gmail.com>
2025-12-30 16:43:20 +0500

    markers

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyA11c06EwJJ7SahxA5FvRF2-_rgI0TPZDwo")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/google_map/googlemap.dart
+++ lib/google_map/googlemap.dart
@@ -8,6 +8,29 @@ class Googlemapclass extends StatefulWidget {
 }
 
 class _GooglemapclassState extends State<Googlemapclass> {
+  late GoogleMapController mapController;
+  // Marker list
+  final Set<Marker> _markers = {};
+
+  // Initial position
+  final LatLng _initialPosition = const LatLng(33.6844, 73.0479); // Islamabad example
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: const MarkerId("marker1"),
+          position: _initialPosition,
+          infoWindow: const InfoWindow(
+            title: "My Location",
+            snippet: "This is marker location",
+          ),
+        ),
+      );
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +39,10 @@ class _GooglemapclassState extends State<Googlemapclass> {
         backgroundColor: Colors.blue,
       ),
       body: GoogleMap(
-          initialCameraPosition: CameraPosition(target: LatLng(73.0000, 83.2222))),
+          initialCameraPosition: CameraPosition(target: LatLng(73.0000, 83.2222),
+          zoom: 14,),
+      mapType: MapType.hybrid,
+      markers: _markers,),
     );
   }
 }
